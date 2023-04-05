22587ecce379d6f7f90a8fa372b9f38399bb9efd
Hannes Benson <hannesbenson@gmail.com>
2023-04-05 13:21:33 +0200

    Add Event#get_instances to get instances of recurring meetings

--- lib/ms_graph_rest/event.rb
+++ lib/ms_graph_rest/event.rb
@@ -15,5 +15,13 @@ module MsGraphRest
       get_path = "#{path}/events/#{id}"
       Response.new(client.get(get_path, query.merge({ '$select' => select }.compact)))
     end
+
+    # 60 Seconds * 60 Minutes * 24 hours * 7 days * 8 weeks
+    EIGHT_WEEKS = 60 * 60 * 24 * 7 * 8
+    def get_instances(id, select: nil, start_date_time: Time.now, end_date_time: Time.now + EIGHT_WEEKS)
+      get_path = "#{path}/events/#{id}/instances?startDateTime=#{start_date_time.iso8601}&endDateTime=#{end_date_time.iso8601}"
+      result = client.get(get_path, query.merge({ '$select' => select }.compact))
+      CalendarView::Response.new(result)
+    end
   end
 end
--- spec/ms_graph_rest/event_spec.rb
+++ spec/ms_graph_rest/event_spec.rb
@@ -19,5 +19,32 @@ module MsGraphRest
         expect(result).to have_attributes(itemId: "Item Id", 'iCalUid' => 'Calendar Uid')
       end
     end
+
+    describe 'Get instances of recurring message' do
+      let(:path) { 'me' }
+      let(:event_id) { 'event_id' }
+      let(:start_date_time) { Time.parse('2023-04-05T10:04:58 UTC') }
+      let(:end_date_time) { Time.parse('2023-05-31T10:04:58 UTC') }
+      let(:body) { File.read("#{File.dirname(__FILE__)}/event/instances.json") }
+
+      before do
+        stub_request(:get, "https://graph.microsoft.com/v1.0/me/events/#{event_id}/instances?endDateTime=2023-05-31T10:04:58Z&startDateTime=2023-04-05T10:04:58Z").to_return(status: 200, body: body, headers: {})
+      end
+
+      it do
+        result = event_query.get_instances(event_id, start_date_time:, end_date_time:)
+        expect(result.size).to eq(3)
+        expect(result.first)
+          .to have_attributes(original_start_time_zone: "originalStartTimeZone-value",
+                              original_end_time_zone: "originalEndTimeZone-value",
+                              i_cal_u_id: '040000008200E00074C5B7101A82E00807E705104AAEFBBC6F67D9010000000000000000100000005D70DF1FCF77AF439519B6F639B957D9',
+                              original_end_time_zone: 'South Africa Standard Time',
+                              original_start_time_zone: 'South Africa Standard Time',
+                              reminder_minutes_before_start: 15,
+                              is_reminder_on: true)
+        expect(result.first.response_status)
+          .to have_attributes(response: 'organizer', time: '0001-01-01T00:00:00Z')
+      end
+    end
   end
 end
